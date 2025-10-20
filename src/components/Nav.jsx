f42d82eb2ebe6ccba41d8d2b472ef4ed19915a61
JoshSald <42002468+JoshSald@users.noreply.github.com>
2025-10-21 00:24:36 +0200

    feat: Add Navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+"use client";
+import {
+  Navbar,
+  NavBody,
+  MobileNav,
+  NavbarLogo,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "./ui/resizable-navbar";
+import { useState } from "react";
+
+export default function Navmenu() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <div className="relative p-4 w-full bg-zinc-200">
+      <Navbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <NavbarLogo />
+        </NavBody>
+
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <NavbarLogo />
+          </MobileNavHeader>
+        </MobileNav>
+      </Navbar>
+    </div>
+  );
+}
